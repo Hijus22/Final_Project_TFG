5b528aaaeb44162aa325d1efa736b00e578e2277
Antonio <amgarin@my.uri.edu>
2016-05-03 19:23:59 -0400

    First Commit

--- Project 499/FirstViewController.swift
+++ Project 499/FirstViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import TwitterKit
+
 
 class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // TODO: Base this Tweet ID on some data from elsewhere in your app
+        TWTRAPIClient().loadTweetWithID("631879971628183552") { (tweet, error) in
+            if let unwrappedTweet = tweet {
+                let tweetView = TWTRTweetView(tweet: unwrappedTweet)
+                tweetView.center = CGPointMake(self.view.center.x, self.topLayoutGuide.length + tweetView.frame.size.height / 2);
+                self.view.addSubview(tweetView)
+            } else {
+                NSLog("Tweet load error: %@", error!.localizedDescription);
+            }
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
